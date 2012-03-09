3442e417741ef7b2b72112b2ba130392c50d0691
ngadiono <cristiano_ondoel@yahoo.com>
2012-03-09 12:40:51 +0700

    menu panel selese

--- application/views/frontend/element/vheader.php
+++ application/views/frontend/element/vheader.php
@@ -2,14 +2,16 @@
     <head>
         <title>Selamat Datang di SMA NEGERI 1 LAMPUNG</title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css"/>
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.tools.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>js/jstyle.js"></script>
     </head>
 
     <body>
     <div class="wrap">
         <div class="header">
             <div class="panel_header">
-                <ul>
-                    <li>Home</li>
+                <ul style="z-index: 2">
+                    <li style="border-bottom-color: white;">Home</li>
                     <li>Profil</li>
                     <li>Guru</li>
                     <li>Siswa</li>
@@ -17,7 +19,7 @@
                     <li>Fitur</li>
                     <li>Elearning</li>
                 </ul>
-                <div class="img_header">
+                <div class="img_header" style="z-index: 0">
 
                 </div>
             </div>
---
+++ js/jstyle.js
@@ -0,0 +1,7 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: root
+ * Date: 3/9/12
+ * Time: 11:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
