7d7b2d3034381d099b26228d12987ad93ce8cee6
ngadiono <cristiano_ondoel@yahoo.com>
2012-03-10 15:31:22 +0700

    benerin tag judul

--- application/controllers/subpage/csub.php
+++ application/controllers/subpage/csub.php
@@ -1,11 +1,93 @@
 <?php
 class Csub extends CI_Controller{
     var $menu;
+
     function __construct(){
         parent::__construct();
     }
     function index(){
-        $data['title']= "Selamat Datang Di SMA NEGERI ABUNG TINGGI";
+        $menu = $_GET['menu'];
+        if($menu=="mot")
+        {
+            $data['title']= "Motto SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="sej")
+        {
+            $data['title']= "Sejarah SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="kep")
+        {
+            $data['title']= "KepSek SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="sar")
+        {
+            $data['title']= "Sarana SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="pres")
+        {
+            $data['title']= "Prestasi SMA NEGERI ABUNG TINGGI";
+        }
+
+        elseif($menu=="dirG")
+        {
+            $data['title']= "Guru SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="aja")
+        {
+            $data['title']= "Materi SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="kal")
+        {
+            $data['title']= "Kalender SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="sil")
+        {
+            $data['title']= "Silabus SMA NEGERI ABUNG TINGGI";
+        }
+
+        elseif($menu=="dirS")
+        {
+            $data['title']= "Siswa SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="eks")
+        {
+            $data['title']= "Ekstrakurikuler SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="prest")
+        {
+            $data['title']= "Prestasi SMA NEGERI ABUNG TINGGI";
+        }
+
+        elseif($menu=="dirA")
+        {
+            $data['title']= "Alumni SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="inf")
+        {
+            $data['title']= "Info SMA NEGERI ABUNG TINGGI";
+        }
+
+        elseif($menu=="age")
+        {
+            $data['title']= "Agenda SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="art")
+        {
+            $data['title']= "Artikel SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="ber")
+        {
+            $data['title']= "Berita SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="opi")
+        {
+            $data['title']= "Opini SMA NEGERI ABUNG TINGGI";
+        }
+        elseif($menu=="tam")
+        {
+            $data['title']= "Tamu SMA NEGERI ABUNG TINGGI";
+        }
+
         $this->load->view('frontend/element/vheader',$data);
 
         $menu = $_GET['menu'];
