df1e7492c111e1466f30f7d26c9f766efc1bbd9f
ngadiono <cristiano_ondoel@yahoo.com>
2012-03-10 11:55:49 +0700

    tata letak list menu selesai

--- application/views/frontend/element/vheader.php
+++ application/views/frontend/element/vheader.php
@@ -14,14 +14,96 @@
     <div class="wrap">
         <div class="header">
             <div class="panel_header">
-                <ul style="z-index: 2">
+                <ul style="z-index: 2" id="menu_list">
                     <li><a href="<?php echo base_url(); ?>index.php/cpage">Home</a></li>
                     <li><a href="<?php echo base_url(); ?>index.php/page/cprofil">Profil</a></li>
                     <li><a href="<?php echo base_url(); ?>index.php/page/cguru">Guru</a></li>
                     <li><a href="<?php echo base_url(); ?>index.php/page/csiswa">Siswa</a></li>
                     <li><a href="<?php echo base_url(); ?>index.php/page/calumni">Alumni</a></li>
                     <li><a href="<?php echo base_url(); ?>index.php/page/cfitur">Fitur</a></li>
-                    <li><a href="<?php echo base_url(); ?>index.php/page/celearning">Elearning</a></li>
+
+                </ul>
+                <ul id="menu_list_hide">
+                    <li>&nbsp;</li>
+                    <li>
+                        <table style="margin-left: 39px;">
+                            <tr>
+                                <td><a href="">Motto, Visi dan Misi</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="">Sejarah Singkat</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="">Kepala Sekolah</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="">Sarana & Prasarana</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="">Prestasi</a></td>
+                            </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table style="margin-left: 127px;">
+                            <tr>
+                                <td><a href="">Direktori Guru</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="">Silabus</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="">Materi Ajar</a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="">Kalender Akademik</a></td>
+                            </tr>
+
+                        </table>
+                    </li>
+                    <li>
+                        <table style="margin-left: 217px;">
+                        <tr>
+                            <td><a href="">Direktori Siswa</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Prestasi Siswa</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Ektrakurikuler</a></td>
+                        </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table style="margin-left: 303px; ">
+                        <tr>
+                            <td><a href="">Direktori Alumni</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Info Alumni</a></td>
+                        </tr>
+                        </table>
+                    </li>
+                    <li>
+                        <table style="margin-left: 389px;">
+                        <tr>
+                            <td><a href="">Agenda</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Artikel</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Berita</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Opini</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="">Buku Tamu</a></td>
+                        </tr>
+                        </table>
+                    </li>
+
                 </ul>
                 <div class="img_header" style="z-index: 0">
 
