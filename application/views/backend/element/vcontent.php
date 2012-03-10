867f5a0b826470f77129b6f7b9f7284348d76528
ngadiono <cristiano_ondoel@yahoo.com>
2012-03-10 21:39:21 +0700

    betulin panel admin

--- application/views/backend/element/vcontent.php
+++ application/views/backend/element/vcontent.php
@@ -26,13 +26,99 @@
         <div class="span10">
             <div class="tab-content">
                 <div class="tab-pane active" id="lA">
-                    <p>Post</p>
+
+                    <?php
+                    $data = array(
+                        'class'=>'well'
+                    );
+                    echo form_open('',$data);
+                    ?>
+                    <table>
+                        <tr>
+                            <td>
+                                <?php
+                                echo form_label('Title','title');
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <?php
+                                $data = array(
+                                    'class'=>'span3',
+                                    'style'=>'height: 25px'
+                                );
+                                echo form_input($data);
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <?php
+
+                                    echo form_label('Note','note');
+                                ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                               <textarea rows="10" cols="40" style="width: 1057px"></textarea>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center">
+                                <?php
+                                    $data = array(
+                                        'class'=>'btn',
+                                        'value'=>'submit'
+                                    );
+                                    echo form_submit($data);
+                                ?>
+                                &nbsp;&nbsp;
+                                <?php
+                                $data = array(
+                                    'class'=>'btn',
+                                    'value'=>'cancel'
+                                );
+                                echo form_submit($data);
+                                ?>
+
+                            </td>
+
+                        </tr>
+                    </table>
+
+                    <?php echo form_close(); ?>
+
+
                 </div>
                 <div class="tab-pane" id="lB">
-                    <p>Setting</p>
+
+                    <form class="well">
+                        <label>Label name</label>
+                        <input type="text" class="span3" placeholder="Type something…"/>
+                        <span class="help-inline">Associated help text!</span>
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            Check me out
+                        </label>
+                        <button type="submit" class="btn">Submit</button>
+                    </form>
+
                 </div>
                 <div class="tab-pane" id="lC">
-                    <p>Users</p>
+
+                    <form class="well">
+                        <label>Label name</label>
+                        <input type="text" class="span3" placeholder="Type something…"/>
+                        <span class="help-inline">Associated help text!</span>
+                        <label class="checkbox">
+                            <input type="checkbox"/>
+                            Check me out
+                        </label>
+                        <button type="submit" class="btn">Submit</button>
+                    </form>
+
                 </div>
             </div>
         </div>
